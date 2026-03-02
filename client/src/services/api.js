8e1b792fa78751c784da228215020a19688fff63
popediv <popooladivine78@gmail.com>
2026-03-02 23:27:30 +0100

    fixed UI bugs and error 404 refresh issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Change to your backend URL
+  baseURL: 'https://e-library-backend-r64m.onrender.com/api', // Change to your backend URL
   withCredentials: true // Include cookies for authentication if your backend uses them
 });
 
